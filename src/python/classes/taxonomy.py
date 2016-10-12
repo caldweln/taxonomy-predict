77b59710b40d0e67a28a001f939a8ba49a7d3f43
caldweln <nick.caldwell94@gmail.com>
2016-10-12 18:32:33 +0200

    label incorporated into parentChain

--- src/python/classes/taxonomy.py
+++ src/python/classes/taxonomy.py
@@ -7,17 +7,16 @@ class TaxonomyTree:
 
     def add(self, nodeLabelChain):
         # pass to root node 
-        self.root.add(TaxTreeNode(nodeLabelChain.pop(), ['root'] + nodeLabelChain))
+        self.root.add(TaxTreeNode(['root'] + nodeLabelChain))
 
     def describe(self):
         print "Taxonomy: " + self.description
         self.root.describe()
 
 class TaxTreeNode:
-    def __init__(self, label, parentChain):
-        self.label = label                      # taxonomy tree node label (category name), unique amongst siblings 
-        self.parentChain = parentChain          # list of node labels, in order from top to bottom 
-        self.children = {}                      # dict of taxonomy tree node, key:node.label 
+    def __init__(self, parentChain):
+        self.parentChain = parentChain          # list of parent node labels, in order from top to bottom 
+        self.children = {}                      # dict of taxonomy tree node, key: last entry in parentChain
         self.isParent = False
         self.isRoot = False
 
@@ -25,25 +24,25 @@ class TaxTreeNode:
         self.isParent = True
         if node.parentChain is None or len(node.parentChain) <= len(self.parentChain):
             # fix parentChain and keep at this level
-            node.parentChain = self.parentChain + [self.label]
+            node.parentChain = self.parentChain
             
-        if cmp(node.parentChain, self.parentChain + [self.label]) == 0:
+        if cmp(node.parentChain, self.parentChain) == 0:
             # keep at this level
-            self.children[node.label] = node
+            self.children[node.parentChain[-1]] = node
         else:
             # pass down to next level
             dstNodeLabel = node.parentChain[len(self.parentChain)+1]
             if not self.children.has_key(dstNodeLabel):
-                self.children[dstNodeLabel] = TaxTreeNode(dstNodeLabel, self.parentChain + [self.label])
+                self.children[dstNodeLabel] = TaxTreeNode(self.parentChain + [dstNodeLabel])
             self.children[nxtParent].add(node)
 
     def describe(self):
         if self.isRoot:
-            print "'" + self.label + "' parent of " + str(len(self.children)) + " children"
+            print "'" + self.parentChain[-1] + "' parent of " + str(len(self.children)) + " children"
         elif self.isParent:
-            print "'" + self.label + "' child of " + str(self.parentChain) + " and parent of " + str(len(self.children)) + " children"
+            print "'" + self.parentChain[-1] + "' at " + str(self.parentChain) + " parent of " + str(len(self.children)) + " children"
         else:
-            print "'" + self.label + "' child of " + str(self.parentChain)
+            print "'" + self.parentChain[-1] + "' at " + str(self.parentChain)
 
         for child in self.children.values():
             child.describe()

015ae4dceb1fbc9735681289e9f00f62ac2aa1a0
nick caldwell <nick.caldwell94@gmail.com>
2016-10-15 13:05:11 +0100

    location replaces parentChain, added support for retrieving location/classifier pairs

--- src/python/classes/taxonomy.py
+++ src/python/classes/taxonomy.py
@@ -3,15 +3,22 @@ class TaxonomyTree:
 
     def __init__(self, description):
         self.description = description
-        self.root = TaxTreeNode(['root'])
+        self.root = TaxTreeNode([])
         self.root.isRoot = True
 
     def add(self, nodeLabelChain):
         # pass to root node
-        self.root.add(TaxTreeNode(['root'] + nodeLabelChain))
+        self.root.add(TaxTreeNode(nodeLabelChain))
 
     def initClassifiers(self, moduleName, classifierName, params):
         self.root.initClassifier(moduleName, classifierName, params)
+
+    def getLocationClassifiers(self):
+        locClsfrs = self.root.getLocationClassifiers()
+        clsfrCount = self.root.getClassifierCount()
+        if len(locClsfrs) != clsfrCount:
+            raise ValueError('getLocationClassifiers / getClassifierCount differ on counts')
+        return locClsfrs
 
     def describe(self):
         print "Taxonomy: " + self.description + " contains "+str(self.root.getDescendentCount()+1)+" nodes, "+str(self.root.getClassifierCount())+" of which have a classifier"
@@ -19,27 +26,31 @@ class TaxonomyTree:
         print "Taxonomy: " + self.description + " contains "+str(self.root.getDescendentCount()+1)+" nodes, "+str(self.root.getClassifierCount())+" of which have a classifier"
 
 class TaxTreeNode:
-    def __init__(self, parentChain):
-        self.parentChain = parentChain          # list of parent node labels, in order from top to bottom
-        self.children = {}                      # dict of taxonomy tree node, key: last entry in parentChain
+    def __init__(self, location):
+        self.location = location                # list of parent node labels, in order from top to bottom
+        self.children = {}                      # dict of taxonomy tree node, key: last entry in location
         self.isParent = False
         self.isRoot = False
         self.classifier = None
 
     def add(self, node):
         self.isParent = True
-        if node.parentChain is None or len(node.parentChain) <= len(self.parentChain):
-            # fix parentChain and keep at this level
-            node.parentChain = self.parentChain + node.parentChain[-1]
+        if node.location is None or len(node.location) == 0:
+            raise ValueError('trying to add a node without location')
+        elif len(node.location) <= len(self.location):
+            # fix location and keep at this level
+            fixedLocation = self.location + node.location[-1]
+            print "Warning: fixing bad location ["+str(node.location)+"] -> ["+str(fixedLocation)+"]"
+            node.location = fixedLocation
 
-        if cmp(node.parentChain[:-1], self.parentChain) == 0:
+        if cmp(node.location[:-1], self.location) == 0:
             # keep at this level
-            self.children[node.parentChain[-1]] = node
+            self.children[node.location[-1]] = node
         else:
             # pass down to next level
-            dstNodeLabel = node.parentChain[len(self.parentChain)]
+            dstNodeLabel = node.location[len(self.location)]
             if not self.children.has_key(dstNodeLabel):
-                self.children[dstNodeLabel] = TaxTreeNode(self.parentChain + [dstNodeLabel])
+                self.children[dstNodeLabel] = TaxTreeNode(self.location + [dstNodeLabel])
             self.children[dstNodeLabel].add(node)
 
     def initClassifier(self, moduleName, classifierName, params):
@@ -62,13 +73,23 @@ class TaxTreeNode:
             num_clsf += child.getClassifierCount()
         return num_clsf
 
+    def getLocationClassifiers(self):
+        result = []
+        if self.classifier is not None:
+            result.append([self.location,self.classifier])
+        for child in self.children.values():
+            descendentResults = child.getLocationClassifiers()
+            if len(descendentResults) > 0:
+                result += (descendentResults)
+        return result
+
     def describe(self):
         if self.isRoot:
-            print "'" + self.parentChain[-1] + "' ["+str(self.classifier)+"] parent of " + str(len(self.children)) + " children"
+            print "<ROOT NODE> ["+str(self.classifier)+"] parent of " + str(len(self.children)) + " children"
         elif self.isParent:
-            print "'" + self.parentChain[-1] + "' ["+str(self.classifier)+"] at " + str(self.parentChain) + " parent of " + str(len(self.children)) + " children"
+            print "'" + self.location[-1] + "' ["+str(self.classifier)+"] at " + str(self.location) + " parent of " + str(len(self.children)) + " children"
         else:
-            print "'" + self.parentChain[-1] + "' ["+str(self.classifier)+"] at " + str(self.parentChain)
+            print "'" + self.location[-1] + "' ["+str(self.classifier)+"] at " + str(self.location)
 
         for child in self.children.values():
             child.describe()

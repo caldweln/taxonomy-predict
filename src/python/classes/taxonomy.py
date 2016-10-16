d4cb9a5c3c905b3063d3eb72e0507c1a273a6b52
nick caldwell <nick.caldwell94@gmail.com>
2016-10-16 17:54:09 +0100

    TaxonomyTree.fit takes work away from test script

--- src/python/classes/taxonomy.py
+++ src/python/classes/taxonomy.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import numpy as np
+import codecs
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn import  cross_validation, preprocessing
 from sklearn import naive_bayes, linear_model, svm, ensemble, neighbors, metrics
@@ -46,9 +48,9 @@ class TaxTreeNode:
             raise ValueError('trying to add a node without location')
         elif len(node.location) <= len(self.location):
             # fix location and keep at this level
-            fixedLocation = self.location + node.location[-1]
-            print "Warning: fixing bad location ["+str(node.location)+"] -> ["+str(fixedLocation)+"]"
+            fixedLocation = self.location + [node.location[-1]]
             node.location = fixedLocation
+            print "Warning: fixing bad location to "+node.getLocationStr()
 
         if cmp(node.location[:-1], self.location) == 0:
             # keep at this level
@@ -91,34 +93,29 @@ class TaxTreeNode:
         return result
 
     def fit(self, x_raw_all, y_raw_all):
-
-        min_per_class_sample_count = 30
+        min_per_class_sample_count = 10
         min_class_count = 2
         rowixs = []
         #
         # Which of the data applies to this node
         #
         y_all = pd.DataFrame(y_raw_all)
+        y_all['label'] = ''
         for ix,row in y_raw_all.iteritems():
             if len(row) > len(self.location) and cmp(row[:len(self.location)],self.location)==0:
-                y_all[ix] = row[len(self.location)]
-                rowixs += [ix]
-                #product_df['nxtCategory'][ix] = row['categories_hierarchy'][len(self.location)]
-
+                y_all['label'][ix] = row[len(self.location)]
+                rowixs.append(ix)
         x_raw = x_raw_all[x_raw_all.index.isin(rowixs)]
-        y = y_all[y_all.index.isin(rowixs)]
-
+        y = y_all['label'][y_all.index.isin(rowixs)]
         #
         # Split into train data for fitting classifier & test data for getting accuracy stat
         #
         x_train_raw, x_test_raw, y_train, y_test = cross_validation.train_test_split(x_raw, y, train_size=0.75,random_state=123)
-
         #
         # Which of this train data is sufficient in numbers
         #
         y_train=y_train.groupby(y_train).filter(lambda x: len(x) > min_per_class_sample_count)
         x_train_raw=x_train_raw[x_train_raw.index.isin(y_train.index)]
-
         #
         # Is there any classes remaining
         #
@@ -126,42 +123,66 @@ class TaxTreeNode:
 
         if len(pruned_child_keys) <= 0:
             #prune this entire branch
+            print "Pruning "+self.getLocationStr()+", not enough samples for any children!"
             return
 
-        if len(pruned_child_keys) > min_class_count:
+        if len(pruned_child_keys) > min_class_count and self.classifier != None:
             #
             # Vectorize and fit the classifier
             #
+            print "Fitting: "+self.getLocationStr()
             vectorizer = CountVectorizer(ngram_range=(1,1), stop_words='english')
             vectorizer.fit(x_train_raw)
             x_train = vectorizer.transform(x_train_raw)
             x_test = vectorizer.transform(x_test_raw)
             self.classifier.fit(x_train, y_train)
             preds = self.classifier.predict(x_test)
-            print "%s:\tAccuracy: %0.3f\tF1 macro: %0.3f"%(self.location,
+            print "%s:\tAccuracy: %0.3f\tF1 macro: %0.3f"%(self.getLocationStr(),
                         metrics.accuracy_score(y_test, preds), metrics.f1_score(y_test, preds, average='macro'))
             self.local_accuracy = metrics.accuracy_score(y_test, preds)
             self.local_f1_score = metrics.f1_score(y_test, preds, average='macro')
             #TODO
             #global metrics, taking into account loss in upper layers
         else:
-            self.classifier = None
-            self.predict_default = y_train.value_counts().index[0]
-            print "Insufficient class count at ["+str(self.location)+"] defaulting prediction to ["+self.predict_default+"]"
-
-
+            self.prune(y_train.value_counts().index[0])
+            print "Skipping: "+self.getLocationStr()+", defaulting prediction to ["+self.getPredictDefaultStr()+"]"
+        #
+        # fit descendent classifiers
+        #
+        x_raw = None
+        y_all = None
+        y = None
         for child_key in pruned_child_keys:
-            self.children[child_key].fit(x_raw, y)
+            if self.children.has_key(child_key):
+                self.children[child_key].fit(x_raw_all, y_raw_all)
+        for child in self.children.values():
+            if child.location[-1] not in pruned_child_keys:
+                child.prune()
 
+    def prune(self, default_predict=None):
+        self.classifier = None
+        self.default_predict = default_predict
+
+    def predict(self, x_data):
+        #check if fitted
+        #check x_data
+        #x_data : {array-like, sparse matrix} Samples
+
+
+    def getLocationStr(self):
+        return str(self.location).encode('ascii', 'ignore')
+
+    def getPredictDefaultStr(self):
+        return self.predict_default.encode('ascii', 'ignore')
 
 
     def describe(self):
         if self.isRoot:
             print "<ROOT NODE> ["+str(self.classifier)+"] parent of " + str(len(self.children)) + " children"
         elif self.isParent:
-            print "'" + self.location[-1] + "' ["+str(self.classifier)+"] at " + str(self.location) + " parent of " + str(len(self.children)) + " children"
+            print "'" + self.location[-1].encode('ascii', 'ignore') + "' ["+str(self.classifier)+"] at " + self.getLocationStr() + " parent of " + str(len(self.children)) + " children"
         else:
-            print "'" + self.location[-1] + "' ["+str(self.classifier)+"] at " + str(self.location)
+            print "'" + self.location[-1].encode('ascii', 'ignore') + "' ["+str(self.classifier)+"] at " + self.getLocationStr()
 
         for child in self.children.values():
             child.describe()
